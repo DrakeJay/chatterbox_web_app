a1ee1e830e219d90e3fe71bb5741d9fe1eb0dc9f
Drake Weller <88635442+DrakeJay@users.noreply.github.com>
2024-11-20 17:05:01 -0500

    trying to get things to work

--- messaging_app/messaging_app/asgi.py
+++ messaging_app/messaging_app/asgi.py
@@ -4,6 +4,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 import chat.routing
 
 
@@ -11,7 +12,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
 
 print("ASGI application loaded successfully")
 
-application = ProtocolTypeRouter({
+application = ASGIStaticFilesHandler(get_asgi_application())({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 def home(request):
     return HttpResponse("Welcome to the Messaging App! Go to <a href='/chat/'>Chat</a>.")
@@ -30,3 +34,8 @@ urlpatterns = [
     path('chat/', include('chat.urls')),
     path('', home, name='home'),  # Add this line for the root URL
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
