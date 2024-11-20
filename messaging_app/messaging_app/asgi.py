fe09c641212021d7843dcd9f272efda17ad07d11
Drake Weller <88635442+DrakeJay@users.noreply.github.com>
2024-11-20 17:23:44 -0500

    FIX

--- messaging_app/messaging_app/asgi.py
+++ messaging_app/messaging_app/asgi.py
@@ -2,9 +2,9 @@
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+
 import chat.routing
 
 
